2d82ec0226bb2540b2955e3b3ea5e0c99d9d6329
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-04 15:58:26 +0900

    first status

---
+++ src/main/java/io/chagchagchag/example/reactive_test_example/ReactiveTestExampleApplication.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.reactive_test_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveTestExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/chagchagchag/example/reactive_test_example/ReactiveTestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chagchagchag.example.reactive_test_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveTestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
